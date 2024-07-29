96e9a9975a6a1a8db74078102dc14e14b28564b5
Maryam <08008maryam@gmail.com>
2024-07-30 03:34:09 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,85 @@
+ alert("maryam")
+ alert("noor")
+ alert("08008maryam@gmail.com")
+ alert("03123832937")
+ alert("00000999")
+ alert("you're learning JavaScript")
+ alert("Welcome to JavaScript Programing")
+
+
+
+// chp 2
+// 1.
+var myVarible;
+// 2.
+var myString;
+myString = "Hello, World!"
+// 3.
+var teamName = "Jawan Team"
+alert(teamName)
+// 4.
+var bestMan = "Charlie"
+bestMan = "john";
+
+
+
+// chp 3
+1.
+var caseQty
+2.
+caseQty = 144;
+3.
+var num = "9";
+4.
+var total;
+total = 5 + 10
+5.
+var merchTotal = 100;
+var shippingCharge = 10;
+var orderTotal = merchTotal + shippingCharge;
+6.
+var myNumber = 7;
+myNumber = myNumber + 3;
+
+
+
+// chp 4
+// var product cost = 3.45;
+ var productcost = 3.45
+// 2.
+// var Name0fband;
+var nameofBand;
+// 3.
+var age = 25;
+// 4. 
+var maryamNOOR;
+// 5.legal
+let myVarible = 10;
+const $price = 100;
+var _name = "Maryam"
+var product1 = "laptop"
+
+// 6.illegal
+// let my Varible = 10
+// const 1price = 100;
+// var var;
+// var my-name;
+
+// chapter 5
+2.
+var num = 20 % 6;
+// the value of num is 2
+3.
+var largeNum = 1000*2000;
+4.
+var a = "6";
+var b = 2;
+var c = a-b;
+alert(c);
+5.
+var reminder = 10 % 3;
+6.
+var a = "5";
+var b = 9;
+var c = a*b;
+alert(c);
